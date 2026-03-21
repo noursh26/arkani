f77d5fde7cadfeda2e5a9a3405ebb20d2987ddce
noursh26 <nour2626sh@gmail.com>
2026-03-21 21:50:34 +0300

    Add placeholder test file for Arkani app to enable CI testing

---
+++ test/app_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Arkani App', () {
+    test('placeholder test passes', () {
+      expect(true, isTrue);
+    });
+  });
+}
